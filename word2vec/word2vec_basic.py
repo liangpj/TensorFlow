2eaeb632b6d59610bb1c5b2c8251176f5f5984d7
liangpj <378058621@qq.com>
2015-12-22 16:20:54 +0800

    最基本的word2vec模型

--- word2vec/word2vec_basic.py
+++ word2vec/word2vec_basic.py
@@ -59,10 +59,10 @@ def build_dataset(words) :
 
     ## 创建一个词汇表 dictionary = (word, id)
     dictionary = dict()
-    for word, _ in words :
-        dictionary[words] = len(dictionary)
+    for word, _ in count :
+        dictionary[word] = len(dictionary)
 
-    data = list()  # word id
+    data = list()  #使用word ID 来表示原来文本中的数据，并且保持原来数据的有序性
     unk_count = 0
     for word in words :
         if word in dictionary :
@@ -78,3 +78,45 @@ def build_dataset(words) :
     return data, count, dictionary, reverse_dictionary
 
 data, count, dictionary, reverse_dictionary = build_dataset(words)
+
+del words  # 将少内存
+print ('Most commom words （+UNK）', count[:10])
+print ("Sample data", data[:10])
+
+
+data_index = 0
+
+
+# 生成训练skip-gram 模型的training batch
+def generate_batch(batch_size, num_skips, skip_window) :
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+
+    span = 2 * skip_window + 1 # [skip_window target skip_window]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span) :
+        buffer.append((data[data_index]))
+        data_index = (data_index + 1) % len(data)
+
+    for i in range(batch_size // num_skips) :
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips) :
+            while target in targets_to_avoid :
+                target = random.randint(0, span-1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+
+    return batch, labels
+
+batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+for i in range(8) :
+    print (batch[i], '->', labels[i, 0])
+    print (reverse_dictionary[batch[i]], '->', reverse_dictionary[labels[i, 0]])
